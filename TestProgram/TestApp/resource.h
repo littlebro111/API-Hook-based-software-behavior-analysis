fa0a66c4572cfd34630003a51fcd9ae96fea4d35
“littlebro111” <295296141@qq.com>
2021-10-19 00:06:55 +0800

    add TestProgram

---
+++ TestProgram/TestApp/resource.h
@@ -0,0 +1,39 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 TestApp.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_TESTAPP_DIALOG              102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_TESTAPP                     107
+#define IDI_SMALL                       108
+#define IDC_TESTAPP                     109
+#define IDR_MAINFRAME                   128
+#define ID_32771                        32771
+#define ID_TEST                         32772
+#define ID_TEST2                        32773
+#define ID_32774                        32774
+#define ID_TEST3                        32775
+#define ID_32776                        32776
+#define ID_TEST4                        32777
+#define ID_32778                        32778
+#define ID_TEST5                        32779
+#define ID_32780                        32780
+#define ID_TEST6                        32781
+#define ID_Menu                         32782
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32783
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
